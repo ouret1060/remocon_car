c1816de89214f840dc7b5123dc253a0ecaf0f6e3
ouret1060 <70791389+ouret1060@users.noreply.github.com>
2020-11-07 18:31:52 +0900

    ピポを追加

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def ab_button():
+    global left, right
+    cur_left2 = 50 if input.button_is_pressed(Button.A) else 0
+    if left != cur_left2:
+        left = cur_left2
+        serial.write_value("left", left)
+        radio.send_value("left", left)
+    cur_right2 = 50 if input.button_is_pressed(Button.B) else 0
+    if right != cur_right2:
+        right = cur_right2
+        serial.write_value("right", right)
+        radio.send_value("right", right)
 def みぎタイア(すぴーど: number):
     # みぎタイアをうごかす
     # すぴーど: -100～100、まいなすでうしろにうごく
@@ -8,6 +20,30 @@ def ひだリタイア(すぴーど: number):
     # すぴーど: -100～100、まいなすでうしろにうごく
     PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED1, 0 if すぴーど >= 0 else 100, 67)
     PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED2, abs(すぴーど), 67)
+def rotation():
+    global left, right
+    cur_left = 50 if input.rotation(Rotation.ROLL) < -30 else 0
+    if input.rotation(Rotation.PITCH) < -30:
+        pass
+    elif input.rotation(Rotation.PITCH) > 30:
+        cur_left = 0 - cur_left
+    else:
+        cur_left = 0
+    if left != cur_left:
+        left = cur_left
+        serial.write_value("left", left)
+        radio.send_value("left", left)
+    cur_right = 50 if input.rotation(Rotation.ROLL) > 30 else 0
+    if input.rotation(Rotation.PITCH) < -30:
+        pass
+    elif input.rotation(Rotation.PITCH) > 30:
+        cur_right = 0 - cur_right
+    else:
+        cur_right = 0
+    if right != cur_right:
+        right = cur_right
+        serial.write_value("right", right)
+        radio.send_value("right", right)
 
 def on_received_value(name, value):
     serial.write_value(name, value)
@@ -20,53 +56,11 @@ def on_received_value(name, value):
         pass
 radio.on_received_value(on_received_value)
 
-すぴーど = 0
-left = 0
 right = 0
-basic.show_icon(IconNames.HEART)
+left = 0
+すぴーど = 0
 PCA9685.reset(67)
-
-def rotation():
-    global left, right
-    cur_left = 50 if input.rotation(Rotation.ROLL) < -30 else 0
-    if input.rotation(Rotation.PITCH) < -30:
-        pass
-    elif input.rotation(Rotation.PITCH) > 30:
-        cur_left = -cur_left
-    else:
-        cur_left = 0
-    if left != cur_left:
-        left = cur_left
-        serial.write_value("left", left)
-        radio.send_value("left", left)
-
-    cur_right = 50 if input.rotation(Rotation.ROLL) > 30 else 0
-    if input.rotation(Rotation.PITCH) < -30:
-        pass
-    elif input.rotation(Rotation.PITCH) > 30:
-        cur_right = -cur_right
-    else:
-        cur_right = 0
-    if right != cur_right:
-        right = cur_right
-        serial.write_value("right", right)
-        radio.send_value("right", right)
-
-def ab_button():
-    global left, right
-    cur_left = 50 if input.button_is_pressed(Button.A) else 0
-    if left != cur_left:
-        left = cur_left
-        serial.write_value("left", left)
-        radio.send_value("left", left)
-
-    cur_right = 50 if input.button_is_pressed(Button.B) else 0
-    if right != cur_right:
-        right = cur_right
-        serial.write_value("right", right)
-        radio.send_value("right", right)
 
 def on_forever():
     ab_button()
-
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,86 +1,76 @@
-function みぎタイア(すぴーど: number) {
-    //  みぎタイアをうごかす
-    //  すぴーど: -100～100、まいなすでうしろにうごく
+function ab_button () {
+    let cur_left2 = input.buttonIsPressed(Button.A) ? 50 : 0
+if (left != cur_left2) {
+        left = cur_left2
+        serial.writeValue("left", left)
+        radio.sendValue("left", left)
+    }
+    let cur_right2 = input.buttonIsPressed(Button.B) ? 50 : 0
+if (right != cur_right2) {
+        right = cur_right2
+        serial.writeValue("right", right)
+        radio.sendValue("right", right)
+    }
+}
+function みぎタイア (すぴーど: number) {
+    // みぎタイアをうごかす
+    // すぴーど: -100～100、まいなすでうしろにうごく
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, すぴーど >= 0 ? 0 : 100, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, Math.abs(すぴーど), 67)
 }
-
-function ひだリタイア(すぴーど: number) {
-    //  ひだリタイアをうごかす
-    //  すぴーど: -100～100、まいなすでうしろにうごく
+function ひだリタイア (すぴーど: number) {
+    // ひだリタイアをうごかす
+    // すぴーど: -100～100、まいなすでうしろにうごく
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, すぴーど >= 0 ? 0 : 100, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, Math.abs(すぴーど), 67)
 }
-
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
+function rotation () {
+    let cur_left = input.rotation(Rotation.Roll) < -30 ? 50 : 0
+if (input.rotation(Rotation.Pitch) < -30) {
+    	
+    } else if (input.rotation(Rotation.Pitch) > 30) {
+        cur_left = 0 - cur_left
+    } else {
+        cur_left = 0
+    }
+    if (left != cur_left) {
+        left = cur_left
+        serial.writeValue("left", left)
+        radio.sendValue("left", left)
+    }
+    let cur_right = input.rotation(Rotation.Roll) > 30 ? 50 : 0
+if (input.rotation(Rotation.Pitch) < -30) {
+    	
+    } else if (input.rotation(Rotation.Pitch) > 30) {
+        cur_right = 0 - cur_right
+    } else {
+        cur_right = 0
+    }
+    if (right != cur_right) {
+        right = cur_right
+        serial.writeValue("right", right)
+        radio.sendValue("right", right)
+    }
+}
+radio.onReceivedValue(function (name, value) {
     serial.writeValue(name, value)
-    //  左に行くときは右を回す、その逆も同じ
+    // 左に行くときは右を回す、その逆も同じ
     if (name == "left") {
         みぎタイア(value)
     } else if (name == "right") {
         ひだリタイア(value)
     } else {
-        
+    	
     }
-    
 })
-let すぴーど = 0
-let left = 0
 let right = 0
-basic.showIcon(IconNames.Heart)
+let left = 0
+let すぴーど = 0
 PCA9685.reset(67)
-function rotation() {
-    
-    let cur_left = input.rotation(Rotation.Roll) < -30 ? 50 : 0
-    if (input.rotation(Rotation.Pitch) < -30) {
-        
-    } else if (input.rotation(Rotation.Pitch) > 30) {
-        cur_left = -cur_left
-    } else {
-        cur_left = 0
-    }
-    
-    if (left != cur_left) {
-        left = cur_left
-        serial.writeValue("left", left)
-        radio.sendValue("left", left)
-    }
-    
-    let cur_right = input.rotation(Rotation.Roll) > 30 ? 50 : 0
-    if (input.rotation(Rotation.Pitch) < -30) {
-        
-    } else if (input.rotation(Rotation.Pitch) > 30) {
-        cur_right = -cur_right
-    } else {
-        cur_right = 0
-    }
-    
-    if (right != cur_right) {
-        right = cur_right
-        serial.writeValue("right", right)
-        radio.sendValue("right", right)
-    }
-    
-}
-
-function ab_button() {
-    
-    let cur_left = input.buttonIsPressed(Button.A) ? 50 : 0
-    if (left != cur_left) {
-        left = cur_left
-        serial.writeValue("left", left)
-        radio.sendValue("left", left)
-    }
-    
-    let cur_right = input.buttonIsPressed(Button.B) ? 50 : 0
-    if (right != cur_right) {
-        right = cur_right
-        serial.writeValue("right", right)
-        radio.sendValue("right", right)
-    }
-    
-}
-
-basic.forever(function on_forever() {
+basic.showIcon(IconNames.Heart)
+music.playTone(2000, music.beat(BeatFraction.Quarter))
+music.playTone(1000, music.beat(BeatFraction.Quarter))
+basic.clearScreen()
+basic.forever(function () {
     ab_button()
 })
